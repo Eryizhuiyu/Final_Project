5b34286601bae5bb8079b489a3c573387293a07f
Shiwei <49450403+Eryizhuiyu@users.noreply.github.com>
2021-04-22 13:30:27 +0800

    change main page route

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,14 @@ let bodyParser = require('body-parser');
 var router = express.Router();
 
 
+
 router.use(express.static('public'));
-
 /*
-
 router.get('/about',(req,res)=>{
     res.sendFile(__dirname+'/about.html');
 });
 
 */
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 
 
 module.exports = router;
