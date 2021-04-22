9cd6ad0ddb0401f143a2c10b0f3689e4f373a202
Shiwei <49450403+Eryizhuiyu@users.noreply.github.com>
2021-04-22 16:42:10 +0800

    try

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,7 @@ var router = express.Router();
 
 
 router.use(express.static('public'));
-/*
-router.get('/about',(req,res)=>{
-    res.sendFile(__dirname+'/about.html');
-});
 
-*/
 
-router.use(bodyParser.urlencode({extended: true}));
-//connect to the database
-let MongoClient = require('mongodb').MongoClient;
-
-let connectionString = "mongodb+ev"
 
 module.exports = router;
