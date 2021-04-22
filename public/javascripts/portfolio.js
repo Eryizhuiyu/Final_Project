7f4471ac7e47063d63e8471eba170dbd0b84cd69
Shiwei <49450403+Eryizhuiyu@users.noreply.github.com>
2021-04-22 11:45:37 +0800

    home&portfolio&travel

---
+++ public/javascripts/portfolio.js
@@ -0,0 +1,45 @@
+
+/*jslint devel: true */
+/*eslint-env browser*/
+/*eslint no-console: 0*/
+/*global
+alert, confirm, console, promp, $
+*/
+
+let url;
+/*play the background music and change the background color when logo is clicked*/
+function changeBackground(url) {
+    document.body.style.backgroundImage = "url";
+    //var audio = document.createElement("audio");
+    //audio.src = "river_sound.wav";
+   //audio.play();
+}
+
+    
+    
+var a = document.querySelector('#esp_bg');
+console.log(a);
+
+$('#esp_bg').hover(function(){
+    document.body.style.backgroundImage = "url(/public/images/portfolio_img/Atlanta/DSC03297.jpeg)";
+});
+
+
+$('#sst_bg').hover(function(){
+    document.body.style.backgroundImage = "url(/public/images/portfolio_img/Beijing/DSC03170.jpeg)";
+});
+
+
+$('#ct_bg').hover(function(){
+    document.body.style.backgroundImage = "url(/public/images/portfolio_img/Glasgow/DSC02952.jpeg)";
+});
+
+
+$('#ph_bg').hover(function(){
+    document.body.style.backgroundImage = "url(/public/images/portfolio_img/Glasgow/DSC02960.jpeg)";
+});
+
+
+$('#dfs_bg').hover(function(){
+    document.body.style.backgroundImage = "url(/public/images/portfolio_img/Beijing/DSC03295.jpeg)";
+});
